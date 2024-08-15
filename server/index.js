7ae69cd51203321f450bed5b1933941c4377ddb7
Suraj11000 <surajshindecs@gmail.com>
2024-08-16 00:33:36 +0530

    sone

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+function getTimestamp() {
+    return new Date().toISOString();
+}
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ timestamp: getTimestamp() }));
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
+});
